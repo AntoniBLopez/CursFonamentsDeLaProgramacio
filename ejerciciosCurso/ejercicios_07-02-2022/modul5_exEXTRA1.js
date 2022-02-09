771bc592ac667b97bc3ee8a1556a9248fb8064d9
Toni B.Lopez local <tonibassols@outlook.es>
2022-02-09 19:31:04 +0100

    Añado ejercicio modul5_exEXTRA1 ( Juego Frio Frio, Caliente Caliente, acertar número aleatorio entre 1 y 500 )

---
+++ ejerciciosCurso/ejercicios_07-02-2022/modul5_exEXTRA1.js
@@ -0,0 +1,74 @@
+let randomNumer = Math.ceil((Math.random() * 500 ))
+
+console.log(randomNumer)
+
+let warmWarmLower = randomNumer - 15
+let warmWarmUpper = randomNumer + 15
+
+let coldColdLower = randomNumer - 50
+let coldColdUpper = randomNumer + 50
+
+const printResult = document.getElementById("printResult")
+const continuePlaying = document.getElementById("continuePlaying")
+
+function start () {
+
+    blankTextOfContinuePlaying()
+
+    const userNumber = Number(document.getElementById("userNumber").value)
+
+    if (validateNumber(userNumber)) {
+        return printResult.innerText = ""
+    }
+    if (letsPlay(randomNumer, userNumber, coldColdLower, coldColdUpper, warmWarmLower, warmWarmUpper) === false) {
+        printResult.innerText = "🥳 Congratulations! You guessed the correct number 🎉"
+        restartGame()
+    }
+}
+
+function validateNumber(userNumber) {
+    if (userNumber === 0) {
+        return true
+    }
+    return false
+}
+
+function letsPlay(randomNumer, userNumber, coldLower, coldUpper, warmLower, warmUpper) {
+
+    if (randomNumer === userNumber) {
+        return false
+    }
+    if (userNumber < randomNumer && userNumber > warmLower) {
+        return printResult.innerText = "🔥Hot Hot🔥 Your number is smaller"
+    }
+    if (userNumber > randomNumer && userNumber < warmUpper) {
+        return printResult.innerText = "🔥Hot Hot🔥 Your number is bigger"
+    }
+    if (userNumber < randomNumer && userNumber < warmLower && userNumber > coldLower) {
+        return printResult.innerText = "😃Warm Warm😃 Your number is smaller"
+    }
+    if (userNumber > randomNumer && userNumber > warmUpper && userNumber < coldUpper) {
+        return printResult.innerText = "😃Warm Warm😃 Your number is biger"
+    }
+    if (userNumber < randomNumer && userNumber < coldLower) {
+        return printResult.innerText = "🧊Cold Cold🧊 Your number is smaller"
+    }
+    if (userNumber > randomNumer && userNumber > coldUpper) {
+        return printResult.innerText = "🧊Cold Cold🧊 Your number is biger"
+    } 
+}
+
+function restartGame() {
+    continuePlaying.innerText = "( If you want to continue playing, continue )"
+    randomNumer = Math.ceil((Math.random() * 500 ))
+
+    warmWarmLower = randomNumer - 15
+    warmWarmUpper = randomNumer + 15
+    
+    coldColdLower = randomNumer - 50
+    coldColdUpper = randomNumer + 50
+
+    console.log(randomNumer)
+}
+
+const blankTextOfContinuePlaying = () => continuePlaying.innerText = ""
