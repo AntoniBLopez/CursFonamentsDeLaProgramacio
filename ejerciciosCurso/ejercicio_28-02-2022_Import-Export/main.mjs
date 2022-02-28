296695191848ba87b3df0a8e823146eb4ecb907f
Toni B.Lopez local <tonibassols@outlook.es>
2022-02-28 17:40:09 +0100

    Añado ejercicio proyecto Import & Export

---
+++ ejerciciosCurso/ejercicio_28-02-2022_Import-Export/main.mjs
@@ -0,0 +1,11 @@
+
+
+import {num1, num2} from './numbers.mjs'
+
+// const result = document.getElementById('result')
+
+// function sum(num1, num2) {
+//     return result.innerText = num1 + num2
+// }
+
+console.log(num1, num2)
---
+++ ejerciciosCurso/ejercicio_28-02-2022_Import-Export/numbers.mjs
@@ -0,0 +1,6 @@
+
+
+const num1 = 5
+const num2 = 8
+
+export {num1, num2}
